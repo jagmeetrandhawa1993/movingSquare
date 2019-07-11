714979a35f0453eed371c4762a7b224dec8bcd82
jagmeetkaur <jagmeetrandhawa1993@gmail.com>
2019-07-11 17:36:35 -0400

    Initial Commit

---
+++ movingSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movingSquare
+//
+//  Created by Jagmeet Kaur on 2019-07-11.
+//  Copyright © 2019 Jagmeet Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
